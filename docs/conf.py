12482997db24860cfc658240ba547c289d083912
Charis Michelakis <cmichelakis@covariance.gr>
2024-12-30 14:16:00 +0200

    change conf.py and pyproject.toml

--- docs/conf.py
+++ docs/conf.py
@@ -19,8 +19,8 @@ sys.path.insert(0, os.path.abspath("../src/"))
 # -- Project information -----------------------------------------------------
 
 project = "ai-python docs"
-copyright = "2022, Daniel Ciborowski"
-author = "Daniel Ciborowski"
+copyright = "2025, CovarianceGR"
+author = "Charis Michelakis"
 
 # The full version, including alpha/beta/rc tags
 release = "0.1.0"
